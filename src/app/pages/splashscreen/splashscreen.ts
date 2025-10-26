938825d794ab5a4e9e07f98aafb7a2ef998e659f
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2025-10-26 21:16:27 +0000

    Remove boilerplate code

---
+++ src/app/pages/splashscreen/splashscreen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splashscreen',
+  imports: [],
+  templateUrl: './splashscreen.html',
+  styleUrl: './splashscreen.css',
+})
+export class Splashscreen {
+
+}

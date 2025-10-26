51574fa6d6e10b14216f2f36b084ac1d923c9907
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2025-10-26 21:37:22 +0000

    Add component files for header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Splashscreen } from "./pages/splashscreen/splashscreen";
+import { CategoriesPage } from "./pages/categories-page/categories-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Splashscreen, CategoriesPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

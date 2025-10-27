0e1a4573ffb87ff8b4dacb100ea625edc1d33238
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2025-10-27 18:11:37 +0000

    Add navigation from NewCategory to all categories

--- src/app/pages/new-category/new-category.ts
+++ src/app/pages/new-category/new-category.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Header } from "../../components/header/header";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-category',
-  imports: [],
+  imports: [Header],
   templateUrl: './new-category.html',
   styleUrl: './new-category.css',
 })
 export class NewCategory {
-
+  private router = inject(Router)
+  handleClick() {
+    this.router.navigate(['/categories'])
+  }
 }

2b1dbb4465afa01e90c51360878c0c9efe96eed4
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2025-10-27 17:33:52 +0000

    Add first routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Splashscreen } from './pages/splashscreen/splashscreen';
+import { CategoriesPage } from './pages/categories-page/categories-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Splashscreen,
+    pathMatch: 'full',
+  },
+  {
+    path: 'categories',
+    component: CategoriesPage,
+    pathMatch: 'full',
+  },
+  {
+    path: 'categories/:categoryId/transactions',
+    component: Splashscreen,
+    pathMatch: 'full',
+  }
+];
--- src/app/pages/splashscreen/splashscreen.ts
+++ src/app/pages/splashscreen/splashscreen.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-splashscreen',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './splashscreen.html',
   styleUrl: './splashscreen.css',
 })

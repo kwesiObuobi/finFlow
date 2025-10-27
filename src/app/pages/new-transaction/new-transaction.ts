3bb7fe1d3b2847185909ce78261122c002ffba39
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2025-10-27 18:38:47 +0000

    Add NewTransaction component

---
+++ src/app/pages/new-transaction/new-transaction.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-transaction',
+  imports: [],
+  templateUrl: './new-transaction.html',
+  styleUrl: './new-transaction.css',
+})
+export class NewTransaction {
+
+}
